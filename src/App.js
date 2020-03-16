4a177ab78a0f5e4fda4bd4c95a8fbc488e3d588b
bhaskrdas090 <bhaskardas090@gmail.com>
2020-03-16 12:33:56 +0530

    Simplest Todo app in App.js Component using Hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, task: "dancing" },
+    { id: 2, task: "teaching" },
+    { id: 3, task: "learning" }
+  ]);
+  const [task, setTask] = useState("");
+  const [editedTodo, setEditedTodo] = useState(null);
+
+  const handleChange = e => {
+    setTask(e.target.value);
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (editedTodo) {
+      let newTodos = [...todos];
+      newTodos[editedTodo].task = task;
+      setTodos(newTodos);
+      setEditedTodo(null);
+    } else {
+      setTodos([...todos, { id: todos.length + 1, task }]);
+    }
+    setTask("");
+  };
+  const handleDelete = id => {
+    const newTodos = todos.filter(todo => todo.id !== id);
+    setTodos(newTodos);
+  };
+  const handleEdit = index => {
+    let newTodos = [...todos];
+    const todo = newTodos[index];
+    setTask(todo.task);
+    setEditedTodo(index);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {todos.map((todo, index) => (
+        <div key={todo.id}>
+          <p>{todo.task}</p>
+          <button onClick={() => handleDelete(todo.id)}>Delete</button>
+          <button onClick={() => handleEdit(index)}>Edit</button>
+        </div>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="todo"
+          id="todo"
+          onChange={handleChange}
+          value={task}
+        />
+        <button>{editedTodo ? "Update" : "Submit"}</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
